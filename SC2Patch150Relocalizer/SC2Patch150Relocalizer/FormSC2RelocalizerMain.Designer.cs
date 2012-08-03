08e1f1efb6ab99548d67287e888d1f933011445a
Simon Liang <no.lhr0909@gmail.com>
2012-08-03 02:30:54 -0400

    Initial commit (v0.0.1)
    Should be good to go.

---
+++ SC2Patch150Relocalizer/SC2Patch150Relocalizer/FormSC2RelocalizerMain.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace SC2Patch150Relocalizer
+{
+    partial class FormSC2RelocalizerMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deDERadio = new System.Windows.Forms.RadioButton();
+            this.enGBRadio = new System.Windows.Forms.RadioButton();
+            this.enSGRadio = new System.Windows.Forms.RadioButton();
+            this.enUSRadio = new System.Windows.Forms.RadioButton();
+            this.esESRadio = new System.Windows.Forms.RadioButton();
+            this.plPLRadio = new System.Windows.Forms.RadioButton();
+            this.koKRRadio = new System.Windows.Forms.RadioButton();
+            this.itITRadio = new System.Windows.Forms.RadioButton();
+            this.frFRRadio = new System.Windows.Forms.RadioButton();
+            this.esMXRadio = new System.Windows.Forms.RadioButton();
+            this.zhTWRadio = new System.Windows.Forms.RadioButton();
+            this.zhCNRadio = new System.Windows.Forms.RadioButton();
+            this.ruRURadio = new System.Windows.Forms.RadioButton();
+            this.plBRRadio = new System.Windows.Forms.RadioButton();
+            this.buttonRelocalize = new System.Windows.Forms.Button();
+            this.browserSC2Folder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // deDERadio
+            // 
+            this.deDERadio.AutoSize = true;
+            this.deDERadio.Location = new System.Drawing.Point(18, 12);
+            this.deDERadio.Name = "deDERadio";
+            this.deDERadio.Size = new System.Drawing.Size(116, 17);
+            this.deDERadio.TabIndex = 0;
+            this.deDERadio.Text = "deDE (EU German)";
+            this.deDERadio.UseVisualStyleBackColor = true;
+            this.deDERadio.CheckedChanged += new System.EventHandler(this.deDERadio_CheckedChanged);
+            // 
+            // enGBRadio
+            // 
+            this.enGBRadio.AutoSize = true;
+            this.enGBRadio.Location = new System.Drawing.Point(18, 35);
+            this.enGBRadio.Name = "enGBRadio";
+            this.enGBRadio.Size = new System.Drawing.Size(113, 17);
+            this.enGBRadio.TabIndex = 1;
+            this.enGBRadio.Text = "enGB (EU English)";
+            this.enGBRadio.UseVisualStyleBackColor = true;
+            this.enGBRadio.CheckedChanged += new System.EventHandler(this.enGBRadio_CheckedChanged);
+            // 
+            // enSGRadio
+            // 
+            this.enSGRadio.AutoSize = true;
+            this.enSGRadio.Location = new System.Drawing.Point(18, 58);
+            this.enSGRadio.Name = "enSGRadio";
+            this.enSGRadio.Size = new System.Drawing.Size(119, 17);
+            this.enSGRadio.TabIndex = 2;
+            this.enSGRadio.Text = "enSG (SEA English)";
+            this.enSGRadio.UseVisualStyleBackColor = true;
+            this.enSGRadio.CheckedChanged += new System.EventHandler(this.enSGRadio_CheckedChanged);
+            // 
+            // enUSRadio
+            // 
+            this.enUSRadio.AutoSize = true;
+            this.enUSRadio.Location = new System.Drawing.Point(18, 81);
+            this.enUSRadio.Name = "enUSRadio";
+            this.enUSRadio.Size = new System.Drawing.Size(114, 17);
+            this.enUSRadio.TabIndex = 3;
+            this.enUSRadio.Text = "enUS (AM English)";
+            this.enUSRadio.UseVisualStyleBackColor = true;
+            this.enUSRadio.CheckedChanged += new System.EventHandler(this.enUSRadio_CheckedChanged);
+            // 
+            // esESRadio
+            // 
+            this.esESRadio.AutoSize = true;
+            this.esESRadio.Location = new System.Drawing.Point(18, 104);
+            this.esESRadio.Name = "esESRadio";
+            this.esESRadio.Size = new System.Drawing.Size(115, 17);
+            this.esESRadio.TabIndex = 4;
+            this.esESRadio.Text = "esES (EU Spanish)";
+            this.esESRadio.UseVisualStyleBackColor = true;
+            this.esESRadio.CheckedChanged += new System.EventHandler(this.esESRadio_CheckedChanged);
+            // 
+            // plPLRadio
+            // 
+            this.plPLRadio.AutoSize = true;
+            this.plPLRadio.Location = new System.Drawing.Point(149, 104);
+            this.plPLRadio.Name = "plPLRadio";
+            this.plPLRadio.Size = new System.Drawing.Size(127, 17);
+            this.plPLRadio.TabIndex = 9;
+            this.plPLRadio.Text = "plPL (EU Portuguese)";
+            this.plPLRadio.UseVisualStyleBackColor = true;
+            this.plPLRadio.CheckedChanged += new System.EventHandler(this.plPLRadio_CheckedChanged);
+            // 
+            // koKRRadio
+            // 
+            this.koKRRadio.AutoSize = true;
+            this.koKRRadio.Location = new System.Drawing.Point(149, 81);
+            this.koKRRadio.Name = "koKRRadio";
+            this.koKRRadio.Size = new System.Drawing.Size(113, 17);
+            this.koKRRadio.TabIndex = 8;
+            this.koKRRadio.Text = "koKR (KR Korean)";
+            this.koKRRadio.UseVisualStyleBackColor = true;
+            this.koKRRadio.CheckedChanged += new System.EventHandler(this.koKRRadio_CheckedChanged);
+            // 
+            // itITRadio
+            // 
+            this.itITRadio.AutoSize = true;
+            this.itITRadio.Location = new System.Drawing.Point(149, 58);
+            this.itITRadio.Name = "itITRadio";
+            this.itITRadio.Size = new System.Drawing.Size(92, 17);
+            this.itITRadio.TabIndex = 7;
+            this.itITRadio.Text = "itIT(EU Italian)";
+            this.itITRadio.UseVisualStyleBackColor = true;
+            this.itITRadio.CheckedChanged += new System.EventHandler(this.itITRadio_CheckedChanged);
+            // 
+            // frFRRadio
+            // 
+            this.frFRRadio.AutoSize = true;
+            this.frFRRadio.Location = new System.Drawing.Point(149, 35);
+            this.frFRRadio.Name = "frFRRadio";
+            this.frFRRadio.Size = new System.Drawing.Size(105, 17);
+            this.frFRRadio.TabIndex = 6;
+            this.frFRRadio.Text = "frFR (EU French)";
+            this.frFRRadio.UseVisualStyleBackColor = true;
+            this.frFRRadio.CheckedChanged += new System.EventHandler(this.frFRRadio_CheckedChanged);
+            // 
+            // esMXRadio
+            // 
+            this.esMXRadio.AutoSize = true;
+            this.esMXRadio.Location = new System.Drawing.Point(149, 12);
+            this.esMXRadio.Name = "esMXRadio";
+            this.esMXRadio.Size = new System.Drawing.Size(118, 17);
+            this.esMXRadio.TabIndex = 5;
+            this.esMXRadio.Text = "esMX (AM Spanish)";
+            this.esMXRadio.UseVisualStyleBackColor = true;
+            this.esMXRadio.CheckedChanged += new System.EventHandler(this.esMXRadio_CheckedChanged);
+            // 
+            // zhTWRadio
+            // 
+            this.zhTWRadio.AutoSize = true;
+            this.zhTWRadio.Location = new System.Drawing.Point(273, 81);
+            this.zhTWRadio.Name = "zhTWRadio";
+            this.zhTWRadio.Size = new System.Drawing.Size(119, 17);
+            this.zhTWRadio.TabIndex = 14;
+            this.zhTWRadio.Text = "zhTW (KR Chinese)";
+            this.zhTWRadio.UseVisualStyleBackColor = true;
+            this.zhTWRadio.CheckedChanged += new System.EventHandler(this.zhTWRadio_CheckedChanged);
+            // 
+            // zhCNRadio
+            // 
+            this.zhCNRadio.AutoSize = true;
+            this.zhCNRadio.Location = new System.Drawing.Point(273, 58);
+            this.zhCNRadio.Name = "zhCNRadio";
+            this.zhCNRadio.Size = new System.Drawing.Size(116, 17);
+            this.zhCNRadio.TabIndex = 13;
+            this.zhCNRadio.Text = "zhCN (CN Chinese)";
+            this.zhCNRadio.UseVisualStyleBackColor = true;
+            this.zhCNRadio.CheckedChanged += new System.EventHandler(this.zhCNRadio_CheckedChanged);
+            // 
+            // ruRURadio
+            // 
+            this.ruRURadio.AutoSize = true;
+            this.ruRURadio.Location = new System.Drawing.Point(273, 35);
+            this.ruRURadio.Name = "ruRURadio";
+            this.ruRURadio.Size = new System.Drawing.Size(115, 17);
+            this.ruRURadio.TabIndex = 12;
+            this.ruRURadio.Text = "ruRU (EU Russian)";
+            this.ruRURadio.UseVisualStyleBackColor = true;
+            this.ruRURadio.CheckedChanged += new System.EventHandler(this.ruRURadio_CheckedChanged);
+            // 
+            // plBRRadio
+            // 
+            this.plBRRadio.AutoSize = true;
+            this.plBRRadio.Location = new System.Drawing.Point(273, 12);
+            this.plBRRadio.Name = "plBRRadio";
+            this.plBRRadio.Size = new System.Drawing.Size(130, 17);
+            this.plBRRadio.TabIndex = 11;
+            this.plBRRadio.Text = "plBR (AM Portuguese)";
+            this.plBRRadio.UseVisualStyleBackColor = true;
+            this.plBRRadio.CheckedChanged += new System.EventHandler(this.plBRRadio_CheckedChanged);
+            // 
+            // buttonRelocalize
+            // 
+            this.buttonRelocalize.Location = new System.Drawing.Point(13, 133);
+            this.buttonRelocalize.Name = "buttonRelocalize";
+            this.buttonRelocalize.Size = new System.Drawing.Size(389, 50);
+            this.buttonRelocalize.TabIndex = 15;
+            this.buttonRelocalize.Text = "Relocalize!";
+            this.buttonRelocalize.UseVisualStyleBackColor = true;
+            this.buttonRelocalize.Click += new System.EventHandler(this.buttonRelocalize_Click);
+            // 
+            // browserSC2Folder
+            // 
+            this.browserSC2Folder.Description = "Please select SC2 Installation Location";
+            this.browserSC2Folder.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // FormSC2RelocalizerMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 188);
+            this.Controls.Add(this.buttonRelocalize);
+            this.Controls.Add(this.zhTWRadio);
+            this.Controls.Add(this.zhCNRadio);
+            this.Controls.Add(this.ruRURadio);
+            this.Controls.Add(this.plBRRadio);
+            this.Controls.Add(this.plPLRadio);
+            this.Controls.Add(this.koKRRadio);
+            this.Controls.Add(this.itITRadio);
+            this.Controls.Add(this.frFRRadio);
+            this.Controls.Add(this.esMXRadio);
+            this.Controls.Add(this.esESRadio);
+            this.Controls.Add(this.enUSRadio);
+            this.Controls.Add(this.enSGRadio);
+            this.Controls.Add(this.enGBRadio);
+            this.Controls.Add(this.deDERadio);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(417, 215);
+            this.MinimumSize = new System.Drawing.Size(417, 215);
+            this.Name = "FormSC2RelocalizerMain";
+            this.Text = "SC2 Patch 1.5.0 Relocalizer";
+            this.Load += new System.EventHandler(this.FormSC2RelocalizerMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton deDERadio;
+        private System.Windows.Forms.RadioButton enGBRadio;
+        private System.Windows.Forms.RadioButton enSGRadio;
+        private System.Windows.Forms.RadioButton enUSRadio;
+        private System.Windows.Forms.RadioButton esESRadio;
+        private System.Windows.Forms.RadioButton plPLRadio;
+        private System.Windows.Forms.RadioButton koKRRadio;
+        private System.Windows.Forms.RadioButton itITRadio;
+        private System.Windows.Forms.RadioButton frFRRadio;
+        private System.Windows.Forms.RadioButton esMXRadio;
+        private System.Windows.Forms.RadioButton zhTWRadio;
+        private System.Windows.Forms.RadioButton zhCNRadio;
+        private System.Windows.Forms.RadioButton ruRURadio;
+        private System.Windows.Forms.RadioButton plBRRadio;
+        private System.Windows.Forms.Button buttonRelocalize;
+        private System.Windows.Forms.FolderBrowserDialog browserSC2Folder;
+    }
+}
+
---
+++ SC2Patch150Relocalizer/SC2Patch150Relocalizer/FormSC2RelocalizerMain.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace SC2Patch150Relocalizer
+{
+    public partial class FormSC2RelocalizerMain : Form
+    {
+        private String originalLanguage = "enUS";
+        private String relocalizeLanguage = "enUS";
+        private String SC2Location = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\StarCraft II\\";
+        private String SC2VariablesLocation = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\StarCraft II\\Variables.txt";
+        private String AppPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+        private String LocaleLocation = "\\Locales\\";
+
+        public FormSC2RelocalizerMain()
+        {
+            //fix app path
+            AppPath = AppPath.Substring(6, AppPath.Length - 6);
+            //init
+            InitializeComponent();
+            //read current locale
+            var varTXTLines = File.ReadAllLines(SC2VariablesLocation);
+            foreach (string line in varTXTLines)
+            {
+                if (line.StartsWith("localeidassets="))
+                {
+                    originalLanguage = line.Substring(line.Length - 4, 4);
+                    ((RadioButton)(Controls.Find(originalLanguage + "Radio", true)[0])).Checked = true;
+                }
+            }
+        }
+
+        private void buttonRelocalize_Click(object sender, EventArgs e)
+        {
+            //backup
+            if (File.Exists(SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive." + originalLanguage))
+            {
+                File.Delete(SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive");
+                File.Delete(SC2Location + ".agent.db");
+                File.Delete(SC2Location + "Launcher.db");
+            }
+            else
+            {
+                File.Move(SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive", SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive." + originalLanguage);
+                File.Move(SC2Location + ".agent.db", SC2Location + ".agent.db." + originalLanguage);
+                File.Move(SC2Location + "Launcher.db", SC2Location + "Launcher.db." + originalLanguage);
+            }
+            //copy in
+            File.Copy(AppPath + LocaleLocation + relocalizeLanguage + "\\Product.SC2Archive", SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive");
+            File.Copy(AppPath + LocaleLocation + relocalizeLanguage + "\\.agent.db", SC2Location + ".agent.db");
+            File.Copy(AppPath + LocaleLocation + relocalizeLanguage + "\\Launcher.db", SC2Location + "Launcher.db");
+            //delete some files
+            if (File.Exists(SC2Location + "Mods\\Core.SC2Mod\\Product.SC2Archive." + originalLanguage))
+            {
+                File.Delete(SC2Location + "Starcraft II Cache.mfil");
+                File.Delete(SC2Location + "Starcraft II.mfil");
+                File.Delete(SC2Location + "Starcraft II.tfil");
+            }
+            else
+            {
+                File.Move(SC2Location + "Starcraft II Cache.mfil", SC2Location + "Starcraft II Cache.mfil." + originalLanguage);
+                File.Move(SC2Location + "Starcraft II.mfil", SC2Location + "Starcraft II.mfil." + originalLanguage);
+                File.Move(SC2Location + "Starcraft II.tfil", SC2Location + "Starcraft II.tfil." + originalLanguage);
+            }
+            //change variable.txt
+            if (!File.Exists(SC2VariablesLocation + "." + originalLanguage))
+            {
+                File.Copy(SC2VariablesLocation, SC2VariablesLocation + "." + originalLanguage);
+            }
+            var varTXTLines = File.ReadAllLines(SC2VariablesLocation);
+            File.Delete(SC2VariablesLocation);
+            var newVarTXT = "";
+            foreach (string line in varTXTLines)
+            {
+                if (line.Equals("localeidassets=" + originalLanguage))
+                {
+                    newVarTXT += ("localeidassets=" + relocalizeLanguage + "\r\n");
+                }
+                else if (line.Equals("localeiddata=" + originalLanguage))
+                {
+                    newVarTXT += ("localeiddata=" + relocalizeLanguage + "\r\n");
+                }
+                else
+                {
+                    newVarTXT += line + "\r\n";
+                }
+            }
+            File.WriteAllText(SC2VariablesLocation, newVarTXT);
+            MessageBox.Show("Relocalization from " + originalLanguage + " to " + relocalizeLanguage + "is finished! Enjoy!");
+        }
+
+        private void deDERadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (deDERadio.Checked)
+            {
+                relocalizeLanguage = "deDE";
+            }
+        }
+
+        private void enGBRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (enGBRadio.Checked)
+            {
+                relocalizeLanguage = "enGB";
+            }
+        }
+
+        private void enSGRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (enSGRadio.Checked)
+            {
+                relocalizeLanguage = "enSG";
+            }
+        }
+
+        private void enUSRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (enUSRadio.Checked)
+            {
+                relocalizeLanguage = "enUS";
+            }
+        }
+
+        private void esESRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (esESRadio.Checked)
+            {
+                relocalizeLanguage = "esES";
+            }
+        }
+
+        private void esMXRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (esMXRadio.Checked)
+            {
+                relocalizeLanguage = "esMX";
+            }
+        }
+
+        private void frFRRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (frFRRadio.Checked)
+            {
+                relocalizeLanguage = "frFR";
+            }
+        }
+
+        private void itITRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (itITRadio.Checked)
+            {
+                relocalizeLanguage = "itIT";
+            }
+        }
+
+        private void koKRRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (koKRRadio.Checked)
+            {
+                relocalizeLanguage = "koKR";
+            }
+        }
+
+        private void plPLRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plBRRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (plBRRadio.Checked)
+            {
+                relocalizeLanguage = "plBR";
+            }
+        }
+
+        private void ruRURadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ruRURadio.Checked)
+            {
+                relocalizeLanguage = "ruRU";
+            }
+        }
+
+        private void zhCNRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (zhCNRadio.Checked)
+            {
+                relocalizeLanguage = "zhCN";
+            }
+        }
+
+        private void zhTWRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (zhTWRadio.Checked)
+            {
+                relocalizeLanguage = "zhTW";
+            }
+        }
+
+        private void FormSC2RelocalizerMain_Load(object sender, EventArgs e)
+        {
+            //check sc2 folder
+            if (Directory.Exists(SC2Location)) return;
+            var result = browserSC2Folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                SC2Location = browserSC2Folder.SelectedPath + "\\";
+            }
+            else
+            {
+                MessageBox.Show("We need to know the SC2 Installation Location!");
+                Application.Exit();
+            }
+        }
+    }
+}
