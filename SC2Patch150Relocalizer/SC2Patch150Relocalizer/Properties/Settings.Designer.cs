ebd06c2a380c81a507f35a60fa3db35790077a42
Simon Liang <no.lhr0909@gmail.com>
2012-08-06 16:37:57 -0400

    Publish v0.3.1

--- SC2Patch150Relocalizer/SC2Patch150Relocalizer/Properties/Settings.Designer.cs
+++ SC2Patch150Relocalizer/SC2Patch150Relocalizer/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace SimonsRelocalizer.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("7")]
+        [global::System.Configuration.DefaultSettingValueAttribute("8")]
         public int VersionCount {
             get {
                 return ((int)(this["VersionCount"]));
@@ -70,7 +70,7 @@ namespace SimonsRelocalizer.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v0.3.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v0.3.1")]
         public string VersionNumber {
             get {
                 return ((string)(this["VersionNumber"]));
