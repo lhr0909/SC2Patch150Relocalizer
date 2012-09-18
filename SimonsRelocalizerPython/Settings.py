0f39c11b8c5daa0ca877cc11b86482fba92699af
Simon Liang <no.lhr0909@gmail.com>
2012-09-18 13:13:07 -0400

    Added Default Paths in Settings.py

--- SimonsRelocalizerPython/Settings.py
+++ SimonsRelocalizerPython/Settings.py
@@ -1,2 +1,8 @@
-SC2_LOCATION = ""
-SC2_VARTXT_LOCATION = ""
+import sys
+import os
+if sys.platform.startswith("darwin"):
+    SC2_LOCATION = "/Applications/StarCraft II/"
+    SC2_VARTXT_LOCATION = os.path.expanduser("~/Library/Application Support/Blizzard/StarCraft II/Variables.txt")
+else:
+    SC2_LOCATION = os.environ["ProgramFiles"] + "/StarCraft II/"
+    SC2_VARTXT_LOCATION = os.path.expanduser("~/StarCraft II/Variables.txt")
